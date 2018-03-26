0c4b701889e38bb7314f9b27ab93f88375e2253b
YINYI <yinyibbk@qq.com>
2018-03-26 15:23:08 +0800

    '修改了一些配置'

--- Default/Default.jsx
+++ Default/Default.jsx
@@ -8,8 +8,8 @@ import { hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore'
 
-// import TodoMain from './containers/TodoMain/index';
-// import Info from './component/Info/index'
+import './common'
+
 import RouterMap from './router/routerMap'
 
 let store = configureStore()
